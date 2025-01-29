9b96753cc00b6e98c4a2bcffc098d31314af4631
zS1L3NT Mac <dev@zectan.com>
2025-01-30 06:20:19 +0800

    ✨ add default maxed out stats

--- engine/game/data.py
+++ engine/game/data.py
@@ -5,3 +5,10 @@ class GameData:
     utility = [False] * 11
 
     categories = (attack, defence, utility)
+
+    def __init__(self):
+        self.attack[7] = True
+        self.attack[11] = True
+        self.defence[1] = True
+        self.defence[3] = True
+        self.utility[7] = True
