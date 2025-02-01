e7d17d5a606c194e205f7e204fbb6b49201a572e
zS1L3NT Mac <dev@zectan.com>
2025-02-02 00:36:30 +0800

    ✨ take a screenshot when the game ends

--- engine/processes/retry.py
+++ engine/processes/retry.py
@@ -1,8 +1,11 @@
+from constants import *
 from regions.game import region
 from ..data import data
 from ..process import Process
+import pyautogui as ui
 import logging
 import time
+import os
 
 class Retry(Process):
     id = "RETRY"
@@ -10,6 +13,10 @@ class Retry(Process):
     def iteration(self):
         if region.playing.modals.game_over.is_present():
             logging.info("💀 Game over modal is present, retrying")
+
+            file_name = os.path.join("logs", "images", f"{time.strftime("%Y-%m-%d %H-%M-%S")}.png")
+            ui.screenshot(region = GAME_REGION).save(file_name)
+
             region.playing.modals.game_over.retry_button.click()
             data.reset()
 
