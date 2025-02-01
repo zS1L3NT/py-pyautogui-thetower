b4d19697d99315fb8fabda9445c00352b0b5b796
zS1L3NT Mac <dev@zectan.com>
2025-02-01 10:04:00 +0800

    ✨ log to stdout too

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from constants import *
 import pyautogui as ui
 import logging
 import time
+import sys
 
 ui.MINIMUM_DURATION = 0.005
 ui.MINIMUM_SLEEP = 0.005
 
 logging.basicConfig(
-    filename = f"logs/{time.strftime("%Y-%m-%d %H-%M-%S")}.log",
-    level = logging.INFO,
-    format = "%(asctime)s - %(threadName)s - %(message)s"
+    handlers=[
+        logging.FileHandler(f"logs/{time.strftime('%Y-%m-%d %H-%M-%S')}.log"),
+        logging.StreamHandler(sys.stdout)
+    ],
+    level=logging.INFO,
+    format="%(asctime)s - %(threadName)s - %(message)s"
 )
 
 engine.start()
